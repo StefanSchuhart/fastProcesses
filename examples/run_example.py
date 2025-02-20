4b010ecf383827cd3328960068c304fe78da07a7
Stefan Schuhart <stefan.schuhart@gv.hamburg.de>
2025-02-20 17:04:41 +0000

    added log config None to use the internal loguru logger

--- examples/run_example.py
+++ examples/run_example.py
@@ -9,4 +9,10 @@ app = OGCProcessesAPI(
 ).get_app()
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=8000,
+        log_config=None,
+        log_level=None,
+    )
