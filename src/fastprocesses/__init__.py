cd0a703cd65803060e46b28f0e5a5211bebc9250
Stefan Schuhart <stefan.schuhart@gv.hamburg.de>
2026-06-01 07:00:33 +0000

    added exports

--- src/fastprocesses/__init__.py
+++ src/fastprocesses/__init__.py
@@ -5,4 +5,19 @@ A library to create a FastAPI-based  OGC API Processes wrapper around existing p
 
 from __future__ import annotations
 
-__all__: list[str] = []
+from fastprocesses.core.exceptions import SerializationError
+from fastprocesses.core.output_protocol import BaseProcessResult, ProcessResult
+from fastprocesses.core.output_schema_resolver import (
+    OutputSchemaResolver,
+    ResolvedOutputFormat,
+)
+from fastprocesses.core.outputs_handler import OutputsHandler
+
+__all__ = [
+    "BaseProcessResult",
+    "OutputSchemaResolver",
+    "OutputsHandler",
+    "ProcessResult",
+    "ResolvedOutputFormat",
+    "SerializationError",
+]
